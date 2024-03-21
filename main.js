d7f4a5843b44495fb8379ce1f30b1773d4b80f58
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 02:17:30 +0500

    Add files via upload
    THIS IS MY PROJECT

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+//let person_Name :string ='Zaniab';
+let person = {
+    age: 20,
+    name: 'Zainab',
+    natinality: 'pakistan',
+    Student: true
+};
+console.log(person);
+let car = {
+    maker: 'toyota',
+    color: 'Black',
+    automatic: true
+};
+console.log(car);
---
+++ main.ts
@@ -0,0 +1,35 @@
+
+//let person_Name :string ='Zaniab';
+
+//const personName_Array :string[] =['Person','Car','Cold drink'];
+
+
+interface person {
+   age :Number,
+   name : string,
+   natinality : string,
+   Student : Boolean
+}
+
+let person :person = {     
+    age : 20,
+     name : 'Zainab',
+     natinality : 'pakistan',
+     Student : true
+}
+
+console.log(person);
+
+interface car {
+   maker :string,
+   color :string,
+   automatic : Boolean
+}
+
+let car ={
+   maker :'toyota',
+   color :'Black',
+    automatic : true
+} 
+
+console.log(car);
